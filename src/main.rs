cec9fcbf666c41a91132cc3652094e789c01661b
Torsten Long <torsten.s.long@gmail.com>
2023-09-03 23:43:18 +0200

    Finish parsing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use core::ops::Range;
 use pulldown_cmark::{Event, Parser, Tag};
 use std::collections::HashMap;
 
+const SPACES_PER_TAB: usize = 4;
+
 fn read_stdin() -> String {
     std::io::stdin()
         .lines()
@@ -156,12 +158,48 @@ fn merge_ranges(ranges: Vec<TextRange>, whitespaces: HashMap<usize, char>) -> Ve
         .collect::<Vec<_>>()
 }
 
+fn ignore_for_indent(ch: &char) -> bool {
+    ch == &'-' || ch == &'*' || ch.is_whitespace()
+}
+
+fn extract_indent(line: &str) -> String {
+    line.chars()
+        .take_while(|el| ignore_for_indent(el))
+        .map(|_| ' ')
+        .collect::<String>()
+}
+
+// Get indent used for each line. This replaces tabs by a certain number of spaces.
+fn indent_for_line(text: &String, spaces_per_tab: usize) -> HashMap<usize, String> {
+    let spaces = (0..spaces_per_tab).map(|_| " ").collect::<String>();
+
+    text.replace("	", &spaces)
+        .split("\n")
+        .enumerate()
+        .map(|(line_nr, line)| (line_nr, extract_indent(line)))
+        .collect::<HashMap<_, _>>()
+}
+
+fn linebreak_positions(text: &String) -> Vec<usize> {
+    text.chars()
+        .enumerate()
+        .filter_map(|(pos, ch)| if ch == '\n' { Some(pos) } else { None })
+        .collect::<Vec<_>>()
+}
+
 fn parse(text: &String) -> Vec<TextRange> {
     let events_and_ranges = Parser::new(&text).into_offset_iter().collect::<Vec<_>>();
     let whitespaces = whitespace_indices(&text);
 
     merge_ranges(to_be_wrapped(events_and_ranges), whitespaces)
 }
+
+// fn wrap(text: &String, wrap_ranges: Vec<TextRange>) -> Vec<String> {
+//     let linebreaks = linebreak_positions(&text);
+//     let indents = indent_for_line(&text, SPACES_PER_TAB);
+//
+//     vec![]
+// }
 
 fn format(text: &String, ranges: Vec<TextRange>) -> String {
     let mut result = String::new();

ab936e2413dbde8e40b8e992d9441ed1f9ad6c0c
Torsten Long <torsten.s.long@gmail.com>
2023-09-04 23:25:37 +0200

    Finish inserting linebreaks even in the presence of common abbreviations

---
+++ src/linebreak.rs
@@ -0,0 +1,97 @@
+use std::collections::HashSet;
+
+pub fn insert_linebreaks_between_sentences(text: &str, indent: &str) -> String {
+    let merged = merge_all_whitespace(text);
+    let sentence_ends = find_sentence_ends(&merged);
+
+    merged
+        .char_indices()
+        .filter_map(|(idx, el)| {
+            if sentence_ends.contains(&Char::Skip(idx)) {
+                None
+            } else if sentence_ends.contains(&Char::Split(idx)) {
+                Some(format!("\n{}{}", indent, el))
+            } else {
+                Some(format!("{}", el))
+            }
+        })
+        .collect::<Vec<_>>()
+        .join("")
+        .trim_end()
+        .to_string()
+}
+
+/// Replace all consecutive whitespace by a single space. That includes line breaks. This is like
+/// piping through `tr -s '[:space:]' ' '` in the shell.
+fn merge_all_whitespace(text: &str) -> String {
+    let mut last_was_whitespace = false;
+
+    text.chars()
+        .filter_map(|el| {
+            if el.is_whitespace() {
+                if last_was_whitespace {
+                    None
+                } else {
+                    last_was_whitespace = true;
+                    Some(' ')
+                }
+            } else {
+                last_was_whitespace = false;
+                Some(el)
+            }
+        })
+        .collect::<String>()
+}
+
+/// Check whether a character is one that may end a sentence and, thus, might warrant the addition
+/// of a line break. This is hard-coded for now but can be outsourced into a config option.
+fn is_sentence_end_marker(ch: char) -> bool {
+    ch == '.' || ch == '!' || ch == '?'
+}
+
+#[derive(Eq, Hash, PartialEq)]
+enum Char {
+    Skip(usize),
+    Split(usize),
+}
+
+/// Check whether the last word is a special one that is knwon as an abbreviation because no line
+/// break should be inserted after one. This is hard-coded so far but can be outsourced into a
+/// config option.
+fn is_keep_word(text: &str, idx: usize) -> bool {
+    // Check 4 character words.
+    let word_4 = &text[idx.checked_sub(3).unwrap_or(idx)..=idx];
+    match word_4 {
+        "etc." | "e.g." | "i.e." => {
+            return true;
+        }
+        _ => {}
+    }
+    // Check 3 character words.
+    let word_3 = &text[idx.checked_sub(2).unwrap_or(idx)..=idx];
+    match word_3 {
+        "cf." => {
+            return true;
+        }
+        _ => {}
+    }
+    false
+}
+
+fn find_sentence_ends(text: &str) -> HashSet<Char> {
+    let lower = text.to_lowercase();
+
+    text.chars()
+        .zip(text.chars().skip(1))
+        .enumerate()
+        .filter_map(|(idx, (first, second))| {
+            let keep_word = is_keep_word(&lower, idx);
+            if second.is_whitespace() && is_sentence_end_marker(first) && !keep_word {
+                Some([Char::Skip(idx + 1), Char::Split(idx + 2)])
+            } else {
+                None
+            }
+        })
+        .flatten()
+        .collect::<HashSet<_>>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod indent;
+mod linebreak;
 mod parse;
 
-use std::collections::HashSet;
-
 use crate::indent::{fill_ranges, spaces, TextRange};
+use crate::linebreak::insert_linebreaks_between_sentences;
 use crate::parse::parse;
 
 fn read_stdin() -> String {
@@ -13,54 +13,6 @@ fn read_stdin() -> String {
         .filter_map(|el| el.ok())
         .collect::<Vec<_>>()
         .join("\n")
-}
-
-/// Replace all consecutive whitespace by a single space. That includes line breaks. This is like
-/// piping through `tr -s '[:space:]' ' '` in the shell.
-fn merge_all_whitespace(text: &str) -> String {
-    let mut last_was_whitespace = false;
-
-    text.chars()
-        .filter_map(|el| {
-            if el.is_whitespace() {
-                if last_was_whitespace {
-                    None
-                } else {
-                    last_was_whitespace = true;
-                    Some(' ')
-                }
-            } else {
-                last_was_whitespace = false;
-                Some(el)
-            }
-        })
-        .collect::<String>()
-}
-
-// fn find_sentence_ends(text: &str) -> HashSet<usize> {
-//     let potential_positions = text.find(pat)
-//
-// }
-
-fn is_sentence_end(ch: char) -> bool {
-    ch == '.' || ch == '!' || ch == '?'
-}
-
-fn insert_linebreaks_between_sentences(text: &str, indent: &str) -> String {
-    merge_all_whitespace(text)
-        .split_inclusive(is_sentence_end)
-        .enumerate()
-        .map(|(_idx, el)| {
-            if el.ends_with(is_sentence_end) {
-                format!("{}\n{}", el.trim_start(), indent)
-            } else {
-                format!("{}", el)
-            }
-        })
-        .collect::<Vec<_>>()
-        .join("")
-        .trim_end()
-        .to_string()
 }
 
 fn format(text: &String, ranges: Vec<TextRange>) -> String {

20a12cafcb9157bde8cb3d350d630f1719818ddb
Torsten Long <torsten.s.long@gmail.com>
2023-09-05 17:36:32 +0200

    Add licence files and headers

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,20 @@
+/* An opinionated line wrapper for markdown files.
+Copyright (C) 2023  Torsten Long
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use core::ops::Range;
 use pulldown_cmark::{Event, Parser, Tag};
 use std::collections::HashMap;

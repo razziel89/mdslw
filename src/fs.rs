9fa906ba2f24026d558ba5c1e9a5ac67c7746119
Torsten Long <torsten.s.long@gmail.com>
2023-09-07 23:13:41 +0200

    Only log when permissions are missing & on broken symlinks for fs crawl

--- src/fs.rs
+++ src/fs.rs
@@ -22,8 +22,6 @@ use walkdir::WalkDir;
 
 pub fn find_files_with_extension(paths: Vec<PathBuf>, extension: &str) -> Result<Vec<PathBuf>> {
     let mut errors = vec![];
-    let mut access_errors = vec![];
-    let mut canonicalise_errors = vec![];
 
     let found = paths
         .into_iter()
@@ -33,21 +31,19 @@ pub fn find_files_with_extension(paths: Vec<PathBuf>, extension: &str) -> Result
             } else if el.is_dir() {
                 Some(
                     // Recursively extract all files with the given extension.
-                    WalkDir::new(el)
+                    WalkDir::new(&el)
                         .into_iter()
-                        // Remember errors when accessing paths.
                         .filter_map(|el| match el {
                             Ok(path) => Some(path),
-                            Err(msg) => {
-                                access_errors.push(format!("{}", msg));
+                            Err(err) => {
+                                eprintln!("{}", err);
                                 None
                             }
                         })
-                        // Remember errors when canonicalising paths.
                         .filter_map(|el| match el.path().canonicalize() {
                             Ok(path) => Some(path),
-                            Err(msg) => {
-                                canonicalise_errors.push(format!("{}", msg));
+                            Err(err) => {
+                                eprintln!("{}: {}", err, el.path().to_string_lossy());
                                 None
                             }
                         })
@@ -62,9 +58,6 @@ pub fn find_files_with_extension(paths: Vec<PathBuf>, extension: &str) -> Result
         })
         .flatten()
         .collect::<Vec<_>>();
-
-    errors.extend(access_errors);
-    errors.extend(canonicalise_errors);
 
     if errors.len() == 0 {
         Ok(found)

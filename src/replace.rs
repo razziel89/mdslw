6d36aa197f308cdfc3221df5c1c47196860c295e
Torsten Long <torsten.s.long@gmail.com>
2023-11-11 00:35:55 +0100

    Simplify space->nbsp replacement
    Spaces cannot occur in URLs. The parser won't recognise such links.

--- src/replace.rs
+++ src/replace.rs
@@ -1,32 +1,11 @@
-use core::ops::Range;
 use pulldown_cmark::{Event, Parser, Tag};
 use std::collections::HashSet;
-
-fn extract_link_text_range(range: Range<usize>, text: &str) -> Option<Range<usize>> {
-    let first = range.start;
-    let snippet = &text[range];
-    let start = snippet.find('[');
-    let end = snippet.find(']');
-    if let (Some(start), Some(end)) = (start, end) {
-        let result = first + start..first + end + 1;
-        eprintln!(
-            "{:?} {:?} {} {}",
-            result.start,
-            result.end,
-            snippet,
-            &text[result.clone()]
-        );
-        Some(result)
-    } else {
-        None
-    }
-}
 
 pub fn replace_spaces_in_links_by_nbsp(text: String) -> String {
     let char_indices_in_links = Parser::new(&text)
         .into_offset_iter()
         .filter_map(|(event, range)| match event {
-            Event::Start(Tag::Link(..)) => extract_link_text_range(range, &text),
+            Event::Start(Tag::Link(..)) => Some(range),
             _ => None,
         })
         .flatten()
